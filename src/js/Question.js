0cad5903bc8186a181718d8b7395288499652cce
Igor <gorbov18@gmail.com>
2018-11-10 17:42:03 +0400

    Question component

---
+++ src/js/Question.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Choices from './Choices';
+
+const Question = ({ currentQuestion, onSelectAnswer }) => {
+  const { question, choices } = currentQuestion;
+  return (
+    <div className={`question`}>
+      <h1>{question}</h1>
+      <Choices choices={choices} onSelectAnswer={onSelectAnswer} />
+    </div>
+  );
+};
+
+Question.propTypes = {
+  currentQuestion: PropTypes.object.isRequired,
+  onSelectAnswer: PropTypes.func.isRequired,
+};
+
+export default Question;
