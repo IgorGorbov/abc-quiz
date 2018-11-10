c8ef8e43000b367430ae07d2926fd97cbe52d0a4
Igor <gorbov18@gmail.com>
2018-11-10 17:42:36 +0400

    NiceButton component

---
+++ src/js/NiceButton.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class NiceButton extends Component {
+  getLetter(index) {
+    const letters = ['A', 'B', 'C'];
+    return letters[index];
+  }
+
+  handleClick = e => {
+    const { choice, onSelectAnswer } = this.props;
+
+    this.button.classList.add('is-selected', 'is-highlighted');
+
+    setTimeout(() => {
+      onSelectAnswer(choice);
+    }, 500);
+  };
+
+  render() {
+    const { choice, index, onSelectAnswer } = this.props;
+    return (
+      <button
+        ref={input => (this.button = input)}
+        className="btn btn-huge"
+        onClick={this.handleClick}
+      >
+        <span className="letter">{this.getLetter(index)}</span> {choice}
+      </button>
+    );
+  }
+}
+
+NiceButton.propTypes = {
+  choice: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  onSelectAnswer: PropTypes.func.isRequired,
+};
+
+export default NiceButton;
